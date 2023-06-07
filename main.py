7629b54dbd4194cb3b1cc016ef8505d454816cba
Juan Luis Alvarez Reyes <luis75@comunidad.unam.mx>
2023-06-06 19:11:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ import streamlit as st
 
 import streamlit as ast
 import scripts.acercade
-import scripts.eda
-import scripts.acd
-import scripts.pca
-import scripts.Cjerar
-import scripts.CKmeans
-import scripts.apriori
-import scripts.Aprono
-import scripts.Aclass
+# import scripts.eda
+# import scripts.acd
+# import scripts.pca
+# import scripts.Cjerar
+# import scripts.CKmeans
+# import scripts.apriori
+# import scripts.Aprono
+# import scripts.Aclass
 
 
 PAGES = {
     "Acerca de": scripts.acercade,#R
-    "Análsis Exploratorio de Datos": scripts.eda,#R
-    "Seleccion de caracteristicas": scripts.acd,#R
-    "Analsis de componentes principales": scripts.pca,
-    "Cluster jerarquico": scripts.Cjerar,#R
-    "Clusterizacion por K-Means": scripts.CKmeans,#R
-    "Reglas de asociacion": scripts.apriori,#Falta imprimir grafica
-    "Pronostico por arboles": scripts.Aprono,#Falta imprimir el arbol y meter prono
-    "Clasificacion por arboles": scripts.Aclass,#Falta imprimir el arbol y meter prono
+    # "Análsis Exploratorio de Datos": scripts.eda,#R
+    # "Seleccion de caracteristicas": scripts.acd,#R
+    # "Analsis de componentes principales": scripts.pca,
+    # "Cluster jerarquico": scripts.Cjerar,#R
+    # "Clusterizacion por K-Means": scripts.CKmeans,#R
+    # "Reglas de asociacion": scripts.apriori,#Falta imprimir grafica
+    # "Pronostico por arboles": scripts.Aprono,#Falta imprimir el arbol y meter prono
+    # "Clasificacion por arboles": scripts.Aclass,#Falta imprimir el arbol y meter prono
 }
 
 def main():
