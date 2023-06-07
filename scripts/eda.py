d5aa3db768c8a76e9841dc0644a67b901e3d7000
Juan Luis Alvarez Reyes <luis75@comunidad.unam.mx>
2023-06-06 19:16:48 -0600

    aa

--- scripts/eda.py
+++ scripts/eda.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 from pandas.tseries.offsets import Hour
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from streamlit.elements.arrow import Data
 
 
--- scripts/pca.py
+++ scripts/pca.py
@@ -3,7 +3,6 @@ import pandas as pd
 from pandas.tseries.offsets import Hour
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from streamlit.elements.arrow import Data
 
 from sklearn.preprocessing import StandardScaler
